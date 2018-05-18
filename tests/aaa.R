b2a6510b029df9c5364c50c0056d779f1ef80728
RobinHankin <hankin.robin@gmail.com>
2018-05-18 15:38:39 +1200

    start work on the test suite

---
+++ tests/aaa.R
@@ -0,0 +1,56 @@
+ ## define some checker functions, call them at the end.  They should
+ ## all return TRUE; these functions also check that the vectorization
+ ## is OK; so for example f(1:10,1:10), f(1:10,1), f(1,1:10) should
+ ## all return TRUE.
+
+checker_x <- function(x){
+  stopifnot(x==x)
+
+  stopifnot(x == x + as.free(0))  
+  stopifnot(x == -(-x))
+  stopifnot(x == +(+x))
+
+  stopifnot(x+x-x == x)
+
+  stopifnot(is.identity(x-x))
+
+  stopifnot(2*x == x+x)
+  stopifnot(3*x == x+x+x)
+  stopifnot(4*x == x+x+x+x)
+  stopifnot(5*x == x+x+x+x+x)
+  stopifnot(6*x == x+x+x+x+x+x)
+
+  stopifnot(x^x==x)
+
+
+  stopifnot(abelianize(abelianize(x)) == abelianize(x))
+  stopifnot(abelianize(abelianize(sum(x)) == abelianize(sum(abelianize(x)))))
+
+  stopifnot(is.identity(sum(x,rev(-x))))
+  stopifnot(is.identity(cumsum(c(x,-rev(x)))[2*length(x)]))
+  stopifnot(cumsum(x)[length(x)] == sum(x))
+
+  return(TRUE)
+}
+
+for(i in 1:100){checker_x(rfree(10,3,2))}
+  
+  
+checker_xy <- function(x,y){
+  stopifnot(x^y == -y+x+y)
+  stopifnot(x+y == x-(-y))
+  return(TRUE)
+}
+
+checker_xyz <- function(x,y,z){
+  stopifnot(x+(y+z) == (x+y)+z)
+  stopifnot(x^(y+z) == (x^y)^z)
+  stopifnot(x^z + y^z == (x+y)^z)
+}
+
+check_abelianize <- function(n){
+  stopifnot(abelianize(sum(abelianize(alpha(sample(n))))) == abc(n))
+  return(TRUE)
+  }
+
+sapply(1:10,check_abel)
