c41b9254b8bbe1fc5cd32423d8fe34aafe0b0ea7
Robin Hankin <hankin.robin@gmail.com>
2021-08-16 11:17:47 +1200

    now three-deep recursion

--- inst/hexicon.R
+++ inst/hexicon.R
@@ -1,3 +1,10 @@
+debug <- FALSE
+if(debug){
+  colors <- c("black","red","blue","green")
+  } else {
+    colors <- rep("black",4)
+  }
+    
 `sixway` <- function(p, l, dopoints=TRUE, ...){
   a <- seq(from = pi/2,by=pi/3,len=6)
   if(dopoints){
@@ -12,18 +19,26 @@
   }
 }
 
+png(file="freegroup_icon.png")
+
 plot(NA,xlim=c(-1.5,1.5),ylim=c(-1.5,1.5),asp=1,axes=FALSE,xlab="",ylab="")
-sixway(c(0,0),1,dopoints=TRUE)
+sixway(c(0,0),1,dopoints=TRUE,col=colors[1])
 
 for(i in 1:6){
   a <- sixway(c(0,0),1,FALSE)[i,]
-  sixway(a,1/3,col="red")
+  sixway(a,1/3,col=colors[2])
   aa <- a 
   for(j in 1:6){
     if(abs(i-j) != 3){
-      sixway(aa +  sixway(c(0,0),1/3,FALSE)[j,],1/9,col="blue")
+      sixway(aa +  sixway(c(0,0),1/3,FALSE)[j,],1/9,col=colors[3])
+      aaa <- aa
+      for(k in 1:6){
+        if(abs(j-k) != 3){
+          sixway(aaa + sixway(c(0,0),1/3,FALSE)[j,]+sixway(c(0,0),1/9,FALSE)[k,],1/27,col=colors[4])
+        }
+      }
     }
   }
 }
    
-    
+dev.off()
